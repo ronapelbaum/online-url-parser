0c610b38b6d2dfacbdb16dfedd3fbe927289cd6d
Ron Apelbaum <ronapelbaum@gmail.com>
2017-03-08 12:20:32 +0200

    add url-parser component

---
+++ src/app/url-parser/url-parser.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'url-parser',
+  templateUrl: './url-parser.component.html',
+  styleUrls: ['./url-parser.component.css']
+})
+export class UrlParserComponent {
+  title = 'online-url-parser';
+}
